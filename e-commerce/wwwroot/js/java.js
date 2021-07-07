530a96f5bdbbf84d8be0b50a373ca95b08d060d0
sarvankm <sarvankm@code.edu.az>
2021-07-08 02:38:36 +0400

    basket dynamic

--- e-commerce/wwwroot/js/java.js
+++ e-commerce/wwwroot/js/java.js
@@ -2,7 +2,12 @@
 const count = document.querySelectorAll(".countproduct");
 const price = document.querySelectorAll(".price");
 let totalamount = document.querySelector(".totalamount");
+for (var i = 0; i < count.length; i++) {
+    var arrr = [];
+    arrr.push(count[i]);
+}
 if (!JSON.parse(localStorage.getItem("basket"))) {
+    console.log("rrrrrrr");
     const arr = [];
     for (var i = 0; i < name.length; i++) {
         const obj = { productName: name[i].textContent, productCount: count[i].innerHTML, productPrice: price[i].innerHTML };
@@ -22,25 +27,46 @@ if (!JSON.parse(localStorage.getItem("basket"))) {
         }
     })
     localStorage.setItem("basket", JSON.stringify(basket));
-
 }
 else {
     const basket = JSON.parse(localStorage.getItem("basket"));
-    for (var i = 0; i < basket.length; i++) {
-        count[i].innerHTML = basket[i].productCount;
 
+    for (var i = 0; i < name.length; i++) {
+
+        if (basket.length == name.length) {
+            count[i].innerHTML = basket[i].productCount;
+            totalamount.innerHTML = localStorage.getItem("totalamount");
+
+        }
+        else {
+            const arr = [];
+            for (var i = 0; i < basket.length; i++) {
+                arr.push(basket[i]);
+            }
+            console.log(arr);
+            const obj = { productName: name[i].innerHTML, productCount: 1, productPrice: price[i].innerHTML };
+            arr.push(obj);
+            localStorage.setItem("basket", JSON.stringify(arr));
+            console.log(parseInt(obj.productPrice));
+            totalamount.innerHTML = parseInt(localStorage.getItem("totalamount")) + parseInt(obj.productPrice);
+            localStorage.setItem("totalamount", totalamount.innerHTML)
+            const basket2 = JSON.parse(localStorage.getItem("basket"));
+
+            for (var i = 0; i < basket2.length; i++) {
+                count[i].innerHTML = basket2[i].productCount;
+
+            }
+        }
     }
-    localStorage.setItem("basket", JSON.stringify(basket));
-    totalamount.innerHTML = localStorage.getItem("totalamount");
-}
 
+}
 
 function myfunction() {
     const basket = JSON.parse(localStorage.getItem("basket"));
     for (var i = 0; i < basket.length; i++) {
-        if (event.target.nextElementSibling.innerHTML === basket[i].productCount) {
+        if (event.target.parentElement.parentElement.children[2].children[0].innerHTML == basket[i].productName) {
             event.target.nextElementSibling.innerHTML = (parseInt(basket[i].productCount) + 1).toString();
-            totalamount.innerHTML = parseInt(totalamount.innerHTML) + parseInt(basket[i].productPrice);
+            totalamount.innerHTML = parseInt(localStorage.getItem("totalamount")) + parseInt(basket[i].productPrice);
             localStorage.setItem("totalamount", totalamount.innerHTML)
         }
     }
@@ -52,3 +78,12 @@ function myfunction() {
         localStorage.setItem("basket", JSON.stringify(arr));
     }
 };
+function myfunc() {
+    const basket = JSON.parse(localStorage.getItem("basket"));
+    const deletedProduct = basket.find(prod => prod.productName == event.target.parentElement.parentElement.nextElementSibling.nextElementSibling.children[0].innerHTML);
+    totalamount.innerHTML = parseInt(totalamount.innerHTML) - parseInt(deletedProduct.productPrice);
+    localStorage.setItem("totalamount", totalamount.innerHTML)
+    basket.splice(basket.indexOf(deletedProduct),1)
+    localStorage.setItem("basket", JSON.stringify(basket));
+    
+}
